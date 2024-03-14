82d69286acac9fd773774c6fc408b528f1f14ce5
unknown <mtrunghieu12@gmail.com>
2024-03-14 10:41:56 +0700

    update admin

--- ecourses/courses/admin.py
+++ ecourses/courses/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from courses.models import Course, Category
+from django.utils.html import mark_safe
 
+class CourseSAdmin(admin.ModelAdmin):
+    list_display = ['id', 'subject', 'created_date', 'update_date']
+    list_filter = ['subject', 'id']
+    search_fields = ['subject']
+    readonly_fields = ['my_image']
+    def my_image(self,course):
+        if course.image:
+            return mark_safe(f"<img width='300' src='/static/{course.image.name}' />")
 # Register your models here.
+
+admin.site.register(Category)
+admin.site.register(Course, CourseSAdmin)
